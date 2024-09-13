60e49c57c6f99e877dc2cbf65b7bec4885884f7c
Dave Arnold <dave@roknsound.com>
2024-09-13 13:42:56 -0700

    Add IAM policy for secretsmanager:GetSecretValue on GitHub runner secret

--- main.tf
+++ main.tf
@@ -24,6 +24,28 @@ resource "aws_secretsmanager_secret_version" "secret" {
   secret_string = local.token
 }
 
+# An error occurred (AccessDeniedException) when calling the GetSecretValue operation: User: arn:aws-us-gov:sts::229685449397:assumed-role/csvd-ghe-runner-EcsTaskRole/5e409858140d4e96ada845c950388fff is not authorized to perform: secretsmanager:GetSecretValue on resource: /github-runners/csvd-ghe-runner/csvd-gh-runner because no identity-based policy allows the secretsmanager:GetSecretValue action
+resource "aws_iam_policy" "secretsmanager_policy" {
+  name        = "secretsmanager-${var.namespace}-${var.hostname}"
+  description = "Policy to allow secretsmanager:GetSecretValue on the specified secret"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "secretsmanager:GetSecretValue",
+        Resource = "/github-runners/${var.namespace}/${var.hostname}"
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "ecs_task_role_policy_attachment" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = aws_iam_policy.secretsmanager_policy.arn
+}
+
 resource "aws_iam_role" "ecs_task_role" {
   name = "${var.namespace}-EcsTaskRole"
 

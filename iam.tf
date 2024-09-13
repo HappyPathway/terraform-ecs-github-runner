c696f058bd7ca771b2694ffb21cd0b2f22abcc35
Dave Arnold <dave@roknsound.com>
2024-09-13 15:26:31 -0700

    Refactor iam.tf to use the one() function for policy_arn assignment

--- iam.tf
+++ iam.tf
@@ -48,6 +48,7 @@ resource "aws_iam_policy" "certs_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_role_certs_policy_attachment" {
+  count      = var.certs == null ? 0 : 1
   role       = aws_iam_role.ecs_task_role.name
   policy_arn = one(aws_iam_policy.certs_policy).arn
 }

df25fff40cb919f72afbe5f4c7b1ad72354bc7fc
Dave Arnold <dave@roknsound.com>
2024-09-20 13:54:42 -0700

    Refactor IAM role policy attachment in iam.tf

--- iam.tf
+++ iam.tf
@@ -23,7 +23,7 @@ resource "aws_iam_role_policy_attachment" "ecs_task_role_secret_policy_attachmen
 
 
 resource "aws_iam_role_policy_attachment" "github_runner_permissions" {
-  count      = var.github_runner_permissions_arn == null ? 0 : 1
+  for_each   = toset(compact([var.github_runner_permissions_arn]))
   role       = aws_iam_role.ecs_task_role.name
   policy_arn = var.github_runner_permissions_arn
 }
